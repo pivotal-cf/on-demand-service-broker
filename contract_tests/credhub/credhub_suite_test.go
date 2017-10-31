3370e0ed2c3ba0ee89f16c3a276d0af95123d84e
Simon Jones <sijones@pivotal.io>
2017-10-31 16:26:10 +0000

    Credhub contract tests use real rootCAs for CH & UAA
    [#152202730]
    
    Signed-off-by: Simon Jones <sijones@pivotal.io>
    Signed-off-by: Kieron Browne <kbrowne@pivotal.io>

--- contract_tests/credhub/credhub_suite_test.go
+++ contract_tests/credhub/credhub_suite_test.go
@@ -2,7 +2,9 @@ package credhub_tests
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/auth"
@@ -10,10 +12,25 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/pivotal-cf/on-demand-service-broker/credhubbroker"
 
+	"log"
 	"testing"
+	"time"
+
+	"crypto/x509"
+
+	"github.com/craigfurman/herottp"
+	"github.com/pivotal-cf/on-demand-service-broker/authorizationheader"
+	"github.com/pivotal-cf/on-demand-service-broker/boshdirector"
+	"github.com/pivotal-cf/on-demand-service-broker/loggerfactory"
+	"github.com/totherme/unstructured"
 )
 
-var dev_env string
+const credhubURL = "https://credhub.service.cf.internal:8844"
+
+var (
+	dev_env string
+	caCerts []string
+)
 
 func TestContractTests(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -22,12 +39,106 @@ func TestContractTests(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 	dev_env = os.Getenv("DEV_ENV")
+	caCerts = extractCAsFromManifest()
 	ensureCredhubIsClean()
 })
 
 var _ = AfterSuite(func() {
 	ensureCredhubIsClean()
 })
+
+type TestingAuthHeaderBuilder struct{}
+
+func (a *TestingAuthHeaderBuilder) NewAuthHeaderBuilder(
+	boshInfo boshdirector.Info,
+	disableSSLCertVerification bool,
+) (boshdirector.AuthHeaderBuilder, error) {
+
+	return authorizationheader.NewClientTokenAuthHeaderBuilder(
+		boshInfo.UserAuthentication.Options.URL,
+		os.Getenv("BOSH_CLIENT_ID"),
+		os.Getenv("BOSH_CLIENT_SECRET"),
+		true,
+		[]byte{},
+	)
+}
+
+func getBoshManifest(deploymentName string) ([]byte, error) {
+	logger := log.New(GinkgoWriter, "", loggerfactory.Flags)
+	certPool, err := x509.SystemCertPool()
+	if err != nil {
+		return []byte{}, err
+	}
+
+	boshClient, err := boshdirector.New(
+		os.Getenv("BOSH_URL"),
+		true,
+		[]byte{},
+		herottp.New(herottp.Config{
+			NoFollowRedirect:                  true,
+			DisableTLSCertificateVerification: true,
+			Timeout: 30 * time.Second,
+		}),
+		&TestingAuthHeaderBuilder{},
+		certPool,
+		logger,
+	)
+
+	if err != nil {
+		return []byte{}, err
+	}
+
+	cfRawManifest, exists, err := boshClient.GetDeployment(deploymentName, logger)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	if !exists {
+		return []byte{}, fmt.Errorf("deployment '%s' not found", deploymentName)
+	}
+	return cfRawManifest, nil
+}
+
+func nameIsCredhubMatcher(data unstructured.Data) bool {
+	val, err := data.GetByPointer("/name")
+	if err != nil {
+		return false
+	}
+	stringVal, err := val.StringValue()
+	if err != nil {
+		return false
+	}
+	return stringVal == "credhub"
+}
+
+func extractCAsFromManifest() []string {
+	cfRawManifest, err := getBoshManifest("cf")
+	Expect(err).NotTo(HaveOccurred())
+
+	cfManifest, err := unstructured.ParseYAML(string(cfRawManifest))
+	Expect(err).NotTo(HaveOccurred())
+
+	igs, err := cfManifest.GetByPointer("/instance_groups")
+	Expect(err).NotTo(HaveOccurred())
+
+	credhubGroup, found := igs.FindElem(nameIsCredhubMatcher)
+	Expect(found).To(BeTrue())
+
+	jobs, err := credhubGroup.GetByPointer("/jobs")
+	Expect(err).NotTo(HaveOccurred())
+	credhubJob, found := jobs.FindElem(nameIsCredhubMatcher)
+	Expect(found).To(BeTrue())
+
+	credhubProperties, err := credhubJob.GetByPointer("/properties/credhub")
+	Expect(err).NotTo(HaveOccurred())
+
+	uaaCert, err := credhubProperties.GetByPointer("/authentication/uaa/ca_certs/0")
+	Expect(err).NotTo(HaveOccurred())
+	credhubCert, err := credhubProperties.GetByPointer("/tls/ca")
+	Expect(err).NotTo(HaveOccurred())
+
+	return []string{uaaCert.UnsafeStringValue(), credhubCert.UnsafeStringValue()}
+}
 
 func testKeyPrefix() string {
 	return fmt.Sprintf("/test-%s", dev_env)
@@ -39,20 +150,17 @@ func makeKeyPath(name string) string {
 
 func ensureCredhubIsClean() {
 	clientSecret := os.Getenv("TEST_CREDHUB_CLIENT_SECRET")
-	if clientSecret == "" {
-		panic("Expected TEST_CREDHUB_CLIENT_SECRET to be set")
-	}
+	Expect(clientSecret).ToNot(BeEmpty(), "Expected TEST_CREDHUB_CLIENT_SECRET to be set")
 
+	Expect(caCerts).ToNot(BeEmpty())
 	credhubClient, err := credhub.New(
-		"https://credhub.service.cf.internal:8844",
-		credhub.SkipTLSValidation(true),
+		credhubURL,
+		credhub.CaCerts(caCerts...),
 		credhub.Auth(auth.UaaClientCredentials("credhub_cli", clientSecret)),
 	)
-	Expect(err).NotTo(HaveOccurred())
-
-	oauth, ok := credhubClient.Auth.(*auth.OAuthStrategy)
-	Expect(ok).To(BeTrue(), "Could not cast Auth to OAuthStrategy")
-	oauth.Login()
+	if err != nil {
+		Fail(fmt.Sprintf("Could not connect to credhub. %s\n", err.Error()))
+	}
 
 	testKeys, err := credhubClient.FindByPath(testKeyPrefix())
 	Expect(err).NotTo(HaveOccurred())
@@ -63,39 +171,50 @@ func ensureCredhubIsClean() {
 
 func credhubCorrectAuth() credhubbroker.CredentialStore {
 	clientSecret := os.Getenv("TEST_CREDHUB_CLIENT_SECRET")
-	if clientSecret == "" {
-		panic("Expected TEST_CREDHUB_CLIENT_SECRET to be set")
-	}
+	Expect(clientSecret).NotTo(BeEmpty(), "Expected TEST_CREDHUB_CLIENT_SECRET to be set")
+
 	credentialStore, err := credhubbroker.NewCredHubStore(
-		"https://credhub.service.cf.internal:8844",
-		credhub.SkipTLSValidation(true),
+		credhubURL,
 		credhub.Auth(auth.UaaClientCredentials("credhub_cli", clientSecret)),
+		credhub.CaCerts(caCerts...),
 	)
-	if err != nil {
-		panic(fmt.Sprintf("Unexpected error: %s\n", err.Error()))
-	}
+	Expect(err).NotTo(HaveOccurred())
 	return credentialStore
+}
+
+func requiredCaCerts(paths ...string) (certs []string, err error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return certs, err
+	}
+
+	for _, path := range paths {
+		caCertPath := filepath.Join(cwd, path)
+		caCert, err := ioutil.ReadFile(caCertPath)
+		if err != nil {
+			return certs, err
+		}
+
+		certs = append(certs, string(caCert))
+	}
+	return
 }
 
 func credhubIncorrectAuth() credhubbroker.CredentialStore {
 	credentialStore, err := credhubbroker.NewCredHubStore(
-		"https://credhub.service.cf.internal:8844",
-		credhub.SkipTLSValidation(true),
+		credhubURL,
+		credhub.CaCerts(caCerts...),
 		credhub.Auth(auth.UaaClientCredentials("credhub_cli", "reallybadsecret")),
 	)
-	if err != nil {
-		panic(fmt.Sprintf("Unexpected error: %s\n", err.Error()))
-	}
+	Expect(err).NotTo(HaveOccurred())
 	return credentialStore
 }
 
 func credhubNoUAAConfig() credhubbroker.CredentialStore {
 	credentialStore, err := credhubbroker.NewCredHubStore(
-		"https://credhub.service.cf.internal:8844",
-		credhub.SkipTLSValidation(true),
+		credhubURL,
+		credhub.CaCerts(caCerts...),
 	)
-	if err != nil {
-		panic(fmt.Sprintf("Unexpected error: %s\n", err.Error()))
-	}
+	Expect(err).NotTo(HaveOccurred())
 	return credentialStore
 }
--- contract_tests/credhub/credhubstore_contract_test.go
+++ contract_tests/credhub/credhubstore_contract_test.go
@@ -8,46 +8,48 @@ import (
 )
 
 var _ = Describe("Credential store", func() {
-	BehavesLikeACredentialStore := func(correctAuth, incorrectAuth, noUAAConfig credhubbroker.CredentialStore) {
-		It("sets and deletes a key-value map credential", func() {
-			keyPath := makeKeyPath("new-name")
-			err := correctAuth.Set(keyPath, map[string]interface{}{"hi": "there"})
-			Expect(err).NotTo(HaveOccurred())
+	It("sets and deletes a key-value map credential", func() {
+		keyPath := makeKeyPath("new-name")
+		correctAuth := credhubCorrectAuth()
+		err := correctAuth.Set(keyPath, map[string]interface{}{"hi": "there"})
+		Expect(err).NotTo(HaveOccurred())
 
-			err = correctAuth.Delete(keyPath)
-			Expect(err).NotTo(HaveOccurred())
-		})
+		err = correctAuth.Delete(keyPath)
+		Expect(err).NotTo(HaveOccurred())
+	})
 
-		It("can store plain string values", func() {
-			keyPath := makeKeyPath("stringy-cred")
-			err := correctAuth.Set(keyPath, "I JUST LOVE CREDENTIALS.")
-			Expect(err).NotTo(HaveOccurred())
-		})
+	It("can store plain string values", func() {
+		keyPath := makeKeyPath("stringy-cred")
+		correctAuth := credhubCorrectAuth()
+		err := correctAuth.Set(keyPath, "I JUST LOVE CREDENTIALS.")
+		Expect(err).NotTo(HaveOccurred())
+	})
 
-		It("produces error when storing other types", func() {
-			keyPath := makeKeyPath("esoteric-cred")
-			err := correctAuth.Set(keyPath, []interface{}{"asdf"})
-			Expect(err).To(MatchError("Unknown credential type"))
-		})
+	It("produces error when storing other types", func() {
+		keyPath := makeKeyPath("esoteric-cred")
+		correctAuth := credhubCorrectAuth()
+		err := correctAuth.Set(keyPath, []interface{}{"asdf"})
+		Expect(err).To(MatchError("Unknown credential type"))
+	})
 
-		It("produces error when authenticating late without UAA config", func() {
-			keyPath := makeKeyPath("doesnt-really-matter")
-			err := noUAAConfig.Delete(keyPath)
-			Expect(err.Error()).To(ContainSubstring("invalid_token"))
-		})
+	It("produces error when authenticating late without UAA config", func() {
+		keyPath := makeKeyPath("doesnt-really-matter")
+		noUAAConfig := credhubNoUAAConfig()
+		err := noUAAConfig.Delete(keyPath)
+		Expect(err.Error()).To(ContainSubstring("invalid_token"))
+	})
 
-		It("produces error when authenticating early without UAA config", func() {
-			err := noUAAConfig.Authenticate()
-			Expect(err).To(HaveOccurred())
-		})
+	It("produces error when authenticating early without UAA config", func() {
+		noUAAConfig := credhubNoUAAConfig()
+		err := noUAAConfig.Authenticate()
+		Expect(err).To(HaveOccurred())
+	})
 
-		It("produces error with incorrect credentials", func() {
-			err := incorrectAuth.Authenticate()
-			Expect(err).To(HaveOccurred())
-		})
-	}
-
-	BehavesLikeACredentialStore(credhubCorrectAuth(), credhubIncorrectAuth(), credhubNoUAAConfig())
+	It("produces error with incorrect credentials", func() {
+		incorrectAuth := credhubIncorrectAuth()
+		err := incorrectAuth.Authenticate()
+		Expect(err).To(HaveOccurred())
+	})
 
 	Describe("CredHub credential store", func() {
 		It("can't be constructed with a bad URI", func() {
