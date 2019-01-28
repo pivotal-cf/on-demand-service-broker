dff1875aa7f09c93935254d228e3811a6aaba88f
Ferran Rodenas <frodenas@gmail.com>
2019-01-28 13:00:30 -0800

    Handle bosh config update at system tests
    [#163388187]

--- system_tests/dynamic_bosh_config/dynamic_bosh_config_test.go
+++ system_tests/dynamic_bosh_config/dynamic_bosh_config_test.go
@@ -5,45 +5,51 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 	"github.com/pivotal-cf/on-demand-service-broker/system_tests/test_helpers/bosh_helpers"
 
 	cf "github.com/pivotal-cf/on-demand-service-broker/system_tests/test_helpers/cf_helpers"
 )
 
 var _ = Describe("DynamicBoshConfig", func() {
-
-	var (
-		serviceId string
-	)
+	var serviceId string
 
 	BeforeEach(func() {
 		serviceId = ""
-	})
-
-	It("handles bosh configs during the lifecycle of a service instance", func() {
-
-		serviceInstanceName = "service" + brokerInfo.TestSuffix
-		boshConfig := fmt.Sprintf(`{"vm_extensions_config": "vm_extensions: [{name: vm-ext%s}]"}`, brokerInfo.TestSuffix)
-
-		cf.CreateService(brokerInfo.ServiceOffering, "redis-with-post-deploy", serviceInstanceName, boshConfig)
-
-		serviceId = "service-instance_" + cf.GetServiceInstanceGUID(serviceInstanceName)
-		configDetails, err := bosh_helpers.GetBOSHConfig("cloud", serviceId)
-		Expect(err).NotTo(HaveOccurred())
-
-		Expect(configDetails).To(ContainSubstring("name: vm-ext" + brokerInfo.TestSuffix))
-
-		// update and check bosh configs
-
-		cf.DeleteService(serviceInstanceName)
-
-		_, err = bosh_helpers.GetBOSHConfig("cloud", serviceId)
-		Expect(err).To(HaveOccurred(), "cloud config wasn't deleted during DeleteService")
 	})
 
 	AfterEach(func() {
 		if serviceId != "" {
 			bosh_helpers.DeleteBOSHConfig("cloud", serviceId)
 		}
+	})
+
+	It("handles bosh configs during the lifecycle of a service instance", func() {
+		serviceInstanceName = "service" + brokerInfo.TestSuffix
+		boshConfig := fmt.Sprintf(`{"vm_extensions_config": "vm_extensions: [{name: vm-ext%s}]"}`, brokerInfo.TestSuffix)
+
+		By("creating a service with the right bosh config")
+		cf.CreateService(brokerInfo.ServiceOffering, "redis-with-post-deploy", serviceInstanceName, boshConfig)
+		serviceId = "service-instance_" + cf.GetServiceInstanceGUID(serviceInstanceName)
+
+		configDetails, err := bosh_helpers.GetBOSHConfig("cloud", serviceId)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(configDetails).To(ContainSubstring("name: vm-ext" + brokerInfo.TestSuffix))
+
+		By("updating the service with a new bosh config")
+		newBoshConfig := fmt.Sprintf(`{"vm_extensions_config": "vm_extensions: [{name: vm-new-ext%s}]"}`, brokerInfo.TestSuffix)
+		Eventually(cf.Cf("update-service", serviceInstanceName, "-c", newBoshConfig), cf.CfTimeout).Should(gexec.Exit(0))
+		cf.AwaitServiceUpdate(serviceInstanceName)
+
+		configDetails, err = bosh_helpers.GetBOSHConfig("cloud", serviceId)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(configDetails).ToNot(ContainSubstring("name: vm-ext" + brokerInfo.TestSuffix))
+		Expect(configDetails).To(ContainSubstring("name: vm-new-ext" + brokerInfo.TestSuffix))
+
+		By("deleting the bosh config when service is deleted")
+		cf.DeleteService(serviceInstanceName)
+
+		_, err = bosh_helpers.GetBOSHConfig("cloud", serviceId)
+		Expect(err).To(HaveOccurred(), "cloud config wasn't deleted during DeleteService")
 	})
 })
