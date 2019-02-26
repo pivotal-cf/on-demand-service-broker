d31762e6ac078c23c2396f0cbedae2a6fe2c3d8f
Derik Evangelista <devangelista@pivotal.io>
2019-02-26 16:57:18 +0000

    refactor schema-tests
    unmarshal into structs for simplicity and do not assume anything about schema contents,
    just that they have _some_ contents
    
    [#164148657]
    
    Signed-off-by: Kieron Browne <kbrowne@pivotal.io>
    Co-authored-by: Kieron Browne <kbrowne@pivotal.io>

--- system_tests/schema_tests/schema_test.go
+++ system_tests/schema_tests/schema_test.go
@@ -23,131 +23,55 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
+	"github.com/pivotal-cf/brokerapi"
 	cf "github.com/pivotal-cf/on-demand-service-broker/system_tests/test_helpers/cf_helpers"
 )
+
+type CFServices struct {
+	Resources []struct {
+		Entity struct {
+			ServicePlansURL string `json:"service_plans_url"`
+		} `json:"entity"`
+	} `json:"resources"`
+}
+
+type CFServicePlans struct {
+	Resources []struct {
+		Entity brokerapi.ServicePlan
+	}
+}
 
 var _ = Describe("Service plan schemas", func() {
 
 	It("fetches the plan schema from cloud foundry", func() {
-		defaultSchema := `{
-  "service_binding": {
-	  "create": {
-		  "parameters": {
-			  "$schema": "http://json-schema.org/draft-04/schema#",
-			  "additionalProperties": false,
-			  "properties": {
-				  "topic": {
-					  "description": "The name of the topic",
-					  "type": "string"
-				  }
-			  },
-			  "type": "object"
-		  }
-	  }
-  },
-  "service_instance": {
-	  "create": {
-		  "parameters": {
-			  "$schema": "http://json-schema.org/draft-04/schema#",
-			  "additionalProperties": true,
-			  "properties": {
-				  "auto_create_topics": {
-					  "description": "Auto create topics",
-					  "type": "boolean"
-				  },
-				  "default_replication_factor": {
-					  "type": "integer",
-					  "description": "Replication factor"
-				  }
-			  },
-			  "type": "object"
-		  }
-	  },
-	  "update": {
-		  "parameters": {
-			  "$schema": "http://json-schema.org/draft-04/schema#",
-			  "additionalProperties": true,
-			  "properties": {
-				  "auto_create_topics": {
-					  "description": "Auto create topics",
-					  "type": "boolean"
-				  },
-				  "default_replication_factor": {
-					  "description": "Replication factor",
-					  "type": "integer"
-				  }
-			  },
-			  "type": "object"
-		  }
-	  }
-  }
-}`
-
 		servicesCurlSession := cf.Cf("curl", fmt.Sprintf("/v2/services?q=label:%s", brokerInfo.ServiceOffering))
 		Eventually(servicesCurlSession).Should(gexec.Exit(0))
-
 		rawJson := servicesCurlSession.Out.Contents()
 
-		entities := getEntities(rawJson)
-		Expect(len(entities)).To(Equal(1))
+		var services CFServices
+		err := json.Unmarshal(rawJson, &services)
+		Expect(err).NotTo(HaveOccurred(), "could not unmarshal CF services response")
+		Expect(len(services.Resources)).To(Equal(1))
 
-		servicePlansURL, ok := entities[0]["service_plans_url"].(string)
-		Expect(ok).To(BeTrue(), "service_plans_url failed to cast as a string")
-		Expect(servicePlansURL).NotTo(Equal(""), "Unable to find service_plans_url")
-
+		servicePlansURL := services.Resources[0].Entity.ServicePlansURL
 		servicePlansCurlSession := cf.Cf("curl", servicePlansURL)
 		Eventually(servicePlansCurlSession).Should(gexec.Exit(0))
+		rawJson = servicePlansCurlSession.Out.Contents()
 
-		rawServicePlans := servicePlansCurlSession.Out.Contents()
-		schemas := getSchemas(rawServicePlans)
+		var servicePlans CFServicePlans
+		err = json.Unmarshal(rawJson, &servicePlans)
+		Expect(err).NotTo(HaveOccurred(), "could not unmarshal CF service plans response")
+		Expect(len(servicePlans.Resources)).To(BeNumerically(">", 0))
 
-		for _, schema := range schemas {
-			actualSchema, err := json.Marshal(schema)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(actualSchema).To(MatchJSON(defaultSchema))
+		schemas := servicePlans.Resources[0].Entity.Schemas
+
+		for _, schema := range []brokerapi.Schema{schemas.Instance.Create, schemas.Instance.Update, schemas.Binding.Create} {
+			createProps, ok := schema.Parameters["properties"]
+			Expect(ok).To(BeTrue(), "schema did not contain properties")
+			createPropsMap, ok := createProps.(map[string]interface{})
+			Expect(ok).To(BeTrue(), "schema properties were not a map")
+			Expect(len(createPropsMap)).To(BeNumerically(">", 0), "schema properties were empty")
 		}
+
 	})
 })
-
-func getResources(rawJson []byte) []map[string]interface{} {
-	d := make(map[string]interface{})
-	json.Unmarshal(rawJson, &d)
-
-	r, ok := d["resources"].([]interface{})
-	Expect(ok).To(BeTrue(), "Resources failed to cast to slice")
-
-	resources := make([]map[string]interface{}, len(r))
-	for i, v := range r {
-		resource, ok := v.(map[string]interface{})
-		Expect(ok).To(BeTrue(), fmt.Sprintf("Resource %d failed to cast to map[string]interface{}", i))
-		resources[i] = resource
-	}
-
-	return resources
-}
-
-func getEntities(rawJson []byte) []map[string]interface{} {
-	r := getResources(rawJson)
-
-	entities := make([]map[string]interface{}, len(r))
-	for i, v := range r {
-		entity, ok := v["entity"].(map[string]interface{})
-		Expect(ok).To(BeTrue(), fmt.Sprintf("Resource %d's entity failed to cast to map[string]interface{}", i))
-		entities[i] = entity
-	}
-
-	return entities
-}
-
-func getSchemas(rawJson []byte) []map[string]interface{} {
-	e := getEntities(rawJson)
-
-	schemas := make([]map[string]interface{}, len(e))
-	for i, v := range e {
-		schema, ok := v["schemas"].(map[string]interface{})
-		Expect(ok).To(BeTrue(), fmt.Sprintf("Entity %d's schema failed to cast to map[string]interface{}", i))
-		schemas[i] = schema
-	}
-
-	return schemas
-}
