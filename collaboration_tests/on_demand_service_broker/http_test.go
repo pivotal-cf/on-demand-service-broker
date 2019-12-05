cb23c52b1b181d092101ba94ecb9f3e056ba52df
Winna Bridgewater <wbridgewater@pivotal.io>
2019-12-05 14:14:04 +0000

    Revert "Update TLSConfig for HTTPS broker on go 1.13"
    This reverts commit a07ab7a0e323b69c2cd7791568e15bb1a31fbbf3.

--- collaboration_tests/on_demand_service_broker/http_test.go
+++ collaboration_tests/on_demand_service_broker/http_test.go
@@ -18,6 +18,7 @@ import (
 	"time"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"github.com/pivotal-cf/brokerapi/v7/domain"
 	brokerConfig "github.com/pivotal-cf/on-demand-service-broker/config"
@@ -65,25 +66,37 @@ var _ = Describe("Server Protocol", func() {
 			Expect(catalog["services"][0].Name).To(Equal(serviceName))
 		})
 
-		It("refuses to respond to TLS 1.1", func() {
+		DescribeTable("can use the desired cipher suites",
+			func(cipher uint16) {
+				response, _, err := doHTTPSRequest(http.MethodGet, fmt.Sprintf("https://%s/v2/catalog", serverURL), caCertFile, []uint16{cipher}, 0)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(response.StatusCode).To(Equal(http.StatusOK))
+				Expect(response.TLS.CipherSuite).To(Equal(cipher))
+			},
+			// The first two cipher suites that Pivotal recommends are not available in Golang
+			// Entry("TLS_DHE_RSA_WITH_AES_128_GCM_SHA256", tls.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256),
+			// Entry("TLS_DHE_RSA_WITH_AES_256_GCM_SHA384", tls.TLS_DHE_RSA_WITH_AES_256_GCM_SHA384),
+			Entry("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256),
+			Entry("TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384),
+		)
+
+		DescribeTable("does not serve when the client uses an unacceptable cipher",
+			func(cipher uint16) {
+				log.SetOutput(GinkgoWriter)
+				_, _, err := doHTTPSRequest(http.MethodGet, fmt.Sprintf("https://%s/v2/catalog", serverURL), caCertFile, []uint16{cipher}, 0)
+				log.SetOutput(os.Stdout)
+				Expect(err).To(MatchError(ContainSubstring("remote error: tls: handshake failure")))
+			},
+			Entry("TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305", tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305),
+			Entry("TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA", tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA),
+			Entry("TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256", tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256),
+		)
+
+		It("refuses to serve TLS 1.1", func() {
 			log.SetOutput(GinkgoWriter)
 			_, _, err := doHTTPSRequest(http.MethodGet, fmt.Sprintf("https://%s/v2/catalog", serverURL), caCertFile, acceptableCipherSuites, tls.VersionTLS11)
 			log.SetOutput(os.Stdout)
 			Expect(err).To(MatchError(ContainSubstring("remote error: tls: protocol version not supported")))
-		})
-
-		It("responds to TLS 1.2", func() {
-			log.SetOutput(GinkgoWriter)
-			_, _, err := doHTTPSRequest(http.MethodGet, fmt.Sprintf("https://%s/v2/catalog", serverURL), caCertFile, acceptableCipherSuites, tls.VersionTLS12)
-			log.SetOutput(os.Stdout)
-			Expect(err).ToNot(HaveOccurred())
-		})
-
-		It("responds to TLS 1.3", func() {
-			log.SetOutput(GinkgoWriter)
-			_, _, err := doHTTPSRequest(http.MethodGet, fmt.Sprintf("https://%s/v2/catalog", serverURL), caCertFile, acceptableCipherSuites, tls.VersionTLS13)
-			log.SetOutput(os.Stdout)
-			Expect(err).ToNot(HaveOccurred())
 		})
 
 		It("does not serve HTTP", func() {
@@ -92,7 +105,6 @@ var _ = Describe("Server Protocol", func() {
 
 			log.SetOutput(GinkgoWriter)
 			resp, err := http.DefaultClient.Do(req)
-			Expect(err).ToNot(HaveOccurred())
 			log.SetOutput(os.Stdout)
 			Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
 		})
