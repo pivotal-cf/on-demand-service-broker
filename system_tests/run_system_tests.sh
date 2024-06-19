152c26c46d3e7690f592f3a0fb9cfd359d630db5
Andrew Garner <garnera@vmware.com>
2024-06-19 11:42:32 -0500

    remove changes to run_system_tests
    This was earlier fiddling trying to get system_tests to run, but this
    change breaks CI.
    
    CI always specifies test suites as "system_tests/X" assuming the broker
    repo as the cwd.
    
    [#186826718](https://www.pivotaltracker.com/story/show/186826718)

--- system_tests/run_system_tests.sh
+++ system_tests/run_system_tests.sh
@@ -14,14 +14,11 @@ if [[ $# -eq 0 ]]; then
   sleep 5
 fi
 
-pwd="$(cd $(dirname "$0"); pwd)"
-
 cf api ${CF_URL} --skip-ssl-validation
 cf logout
 cf auth $CF_USERNAME $CF_PASSWORD
 cf target -o ${CF_ORG} -s ${CF_SPACE} # must already exist
 
-cd "${pwd}"
 go run github.com/onsi/ginkgo/v2/ginkgo -r -v \
   --flake-attempts="${RETRY_ATTEMPTS:-1}" \
   --randomize-suites \
@@ -31,4 +28,3 @@ go run github.com/onsi/ginkgo/v2/ginkgo -r -v \
   --fail-on-pending \
   --skip-package=upgrade_deployment_tests \
   "$@"
-cd -
